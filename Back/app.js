8dfbb1ad71c2122034d6d5b02bd3fe9bba8ca766
Jamie <ackland2307@gmail.com>
2021-03-14 21:28:02 +0000

    initial commit

---
+++ Back/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected to DB")
+);
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error(error));
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
