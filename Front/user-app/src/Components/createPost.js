c10b40e0e299bef4492a8324cd19a1bda0489e62
Jamie <ackland2307@gmail.com>
2021-03-28 13:10:55 +0100

    create post started

--- Front/user-app/src/Components/createPost.js
+++ Front/user-app/src/Components/createPost.js
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+
+const CreatePostContainer = styled.div`
+  border: 1px solid black;
+  width: auto;
+  height: 10rem;
+`;
+
+const PostForm = styled.form``;
+
 const CreatePost = () => {
-  return <div>create post</div>;
+  return (
+    <CreatePostContainer>
+      <PostForm>
+        <input />
+        <button>Submit</button>
+      </PostForm>
+    </CreatePostContainer>
+  );
 };
 
 export default CreatePost;
--- Front/user-app/src/Components/home.js
+++ Front/user-app/src/Components/home.js
@@ -1,8 +1,6 @@
 import CreatePost from "../Components/createPost";
 import { Homepage } from "../Styles/homeStyles";
 
-import NavBar from "./Widgets/NavBar";
-
 const HomePage = () => {
   return (
     <Homepage>
