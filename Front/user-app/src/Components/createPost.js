520e1e4e0aface4a02de4379eb247062560bbb52
Jamie <ackland2307@gmail.com>
2021-03-28 13:47:50 +0100

    styling added to submit

--- Front/user-app/src/Components/createPost.js
+++ Front/user-app/src/Components/createPost.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const CreatePostContainer = styled.div`
   border: 1px solid black;
+  border-radius: 0.5rem;
   width: auto;
   height: 10rem;
 `;
@@ -9,11 +11,24 @@ const CreatePostContainer = styled.div`
 const PostForm = styled.form``;
 
 const CreatePost = () => {
+  const [postContent, setPostContent] = useState("");
+
+  const click = (e) => {
+    e.preventDefault();
+    console.log(postContent);
+  };
+
   return (
     <CreatePostContainer>
       <PostForm>
-        <input />
-        <button>Submit</button>
+        <input onChange={(e) => setPostContent(e.target.value)} />
+        <button
+          onClick={(e) => {
+            click(e);
+          }}
+        >
+          Submit
+        </button>
       </PostForm>
     </CreatePostContainer>
   );
